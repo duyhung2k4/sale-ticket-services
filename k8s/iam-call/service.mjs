ac9ce9688bc420c5f7104f1194a24ad47336a284
root <root@MSI.localdomain>
2025-09-25 09:53:39 +0700

    [Remove] service iam iam-call

--- k8s/iam-call/service.mjs
+++
@@ -1,6 +0,0 @@
-try {
-    const buildHelm = await $`helm upgrade --install iam-call -f k8s/values.yaml -n dev k8s`;
-    console.log(buildHelm.stdout)
-} catch (error) {
-    console.log("ERROR: ", error);
-}
